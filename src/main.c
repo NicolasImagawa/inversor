aeb66260ce1ec47cc824b656e1261940960fed97
NicolasImagawa <nicimagawa@gmail.com>
2020-03-25 20:46:35 -0300

    Refiz o lab 1

--- src/main.c
+++ src/main.c
@@ -6,100 +6,98 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-	/*input_str recebe a string de entrada pelo terminal e output_str devolve a string invertida.*/
-	unsigned char input_str[300];
-	unsigned char output_str[300];
+int main(){
 	
-	/*length salva o tamanho da string de entrada.*/
-	int length;
+	/*String para armazenar os dados de entrada.*/
+	unsigned char input_str[100];
 	
-	/*
-	counter eh a referencia para as iteracoes do loop "for" externo deste codigo.
-	Da mesma maneira, counter2 se refere as iteracoes do "for" interno.
-	*/
-	int counter;
-	int counter2;
-	/*
-	Subcounter atua como um contador de caracteres ate que surja um espaco em braco na string.
-	Tal variavel foi implementada para dividir a string de entrada e inverter as palavras do
-	modo desejado.
-	*/
-	int subcounter;
+	/*Como ha casos com mais de uma palavra para ser invertida, fez-se uma matriz onde cada palavra
+	eh armazenada em uma de suas linhas, basicamente, um vetor de strings.*/
+	unsigned char str_vect[100][100];
 	
-	/*
-	sub_str_end serve de referencia para determinar o limite inferior que sera escrito na string
-	de saida. Ou seja, quando ha mais de uma palavra no vetor de entrada, deve-se atualizar a
-	posicao de escrita na string de saida respeitando a ordem desejada.
-	*/
-	int sub_str_end;
+	/*Row e column sao os indicadores de linha e coluna da matriz respectivamente.*/
+	int row = 0;
+	int column = 0;
 	
-	/*
-	aux foi criada para escrever a palavra na ordem contraria a contagem do loop, para que fosse
-	possivel inverter uma parte da string de entrada.
-	*/
-	int aux;
+	/*Para as palavras invertidas, se usa outra matriz.*/
+	unsigned char out_str_vect[100][100];
 	
-	/*
-	scanf esta escrito assim para que tambem detectasse espacos em branco.
-	*/
+	/*Len serve para armazenar o tamanho de cada string.*/
+	int len = 0;
+	
+	/*Counter fara a contagem de letra apos letra de uma string de entrada.*/
+	int counter = 0;
+	
+	/*Rev_counter faz a contagem reversa quando for necessario escrever dados na matriz de saida.*/
+	int rev_counter = 0;
+	
+	/*X e Y sao os contadores para a posicao na matriz ao realizar o processo de inverter os dados de entrada.*/
+	int x = 0;
+	int y = 0;
+	
+	/*Scanf para a entrada.*/
 	scanf("%[^\n]", input_str);
+
+
+	/*Enquanto o fim da string de entrada nao ocorre.*/
 	
-	length = strlen(input_str);
-	
-	/*
-	Inicialmente aux e sub_str_end sao 0.
-	*/
-	aux = 0;
-	sub_str_end = 0;
-	
-	/*
-	o for externo tem a funcao de contar o numero de caracteres antes de um espaco em branco.
-	*/
-	for(counter = 0; counter < length; counter++)
-	{
-		if(input_str[counter] != 32)
-		{
-			subcounter++;
+	while(input_str[counter] != '\0'){
+		
+		/*Se o char em questao nao eh um espaco em branco
+		escreve-se o elemento na matriz de strings e passa-se para o proximo char da palavra.*/
+		if(input_str[counter] != ' '){
+			str_vect[row][column] = input_str[counter];
+			column = column + 1;
 		}
 		
-		/*
-		Ao detectar um espaco em branco, interpreta-se que a primeira palavra da string acabou
-		e assim, inverte-se a palavra.
-		*/
-		if((input_str[counter] == 32)||(counter == length - 1)) //
-		{
-			for(counter2 = subcounter - 1; counter2 >= sub_str_end; counter2 --)
-			{
-				output_str[aux] = input_str[counter2];
-				aux = aux + 1;
-			}
-			
-			/*
-			Apos escrever a palavra invertida na string de saida, adiciona-se um espaco em
-			branco para respeitar o resultado requisitado.
-			*/
-			if(counter < (length - 1))
-			{
-				output_str[aux] = 32;
-			}
-			/*
-			Devido também ao espaco em branco, subcounter e aux devem ser atualizados de
-			maneira a evitar a sobrescrita na string de saida.
-			*/
-			subcounter = subcounter + 1;
-			aux = aux + 1;
-			
-			/*Atualiza-se sub_str_end para o novo limite inferior de escrita.*/
-			sub_str_end = subcounter;
+		/*Em caso de espaco em branco, entende-se que ha uma palavra nova e assim, uma nova linha da matriz
+		sera usada para escrita de caracteres.*/
+		else if(input_str[counter] == ' '){
+			str_vect[row][column] = '\0';
+			row = row + 1;
+			column = 0;
 		}
+		
+		/*Incremento da posicao da string de entrada.*/
+		counter++;
 	}
 	
-	/*Print da string invertida.*/
-	for(counter = 0; counter < length; counter++)
-	{
-		printf("%c", output_str[counter]);
+	/*Enquanto o contador x for menor do que o numero de linhas escritas da matriz de strings.*/
+	while(x <= row){
+		
+		/*Usa-se len para saber o numero de letras da palavra em questao*/
+		len = strlen(str_vect[x]);
+		/*Atribui-se a rev_counter o inicio da contagem reversa.*/
+		rev_counter = len - 1;
+		
+		/*Enquanto o contador y for menor do o numero de letras de uma palavra da matriz.*/
+		while(y < len){
+			
+			/*Inversao de uma palavra da matriz e escrita da invertida na matriz de saida.*/
+			out_str_vect[x][rev_counter] = str_vect[x][y];
+			rev_counter = rev_counter - 1;
+			y = y + 1;
+		}
+		
+		/*Com y = 0, eh possivel ler a proxima palavra da esquerda para a direita.*/
+		y = 0;
+		
+		/*Inicio da analise da proxima palavra.*/
+		x = x + 1;
+	}
+	
+	/*Print da matriz com palavras invertidas.
+	Como os espacos nao foram adicionados a matriz inicial, aqui tambem eh adicionado
+	um espaco em branco entre duas palavras.*/
+	for(x = 0; x <= row; x ++){
+		if(x < row){
+			printf("%s", out_str_vect[x]);
+			printf("%c", 32);
+		}
+		
+		else if(x == row){
+			printf("%s", out_str_vect[x]);
+		}
 	}
 	printf("\n");
 	return 0;
